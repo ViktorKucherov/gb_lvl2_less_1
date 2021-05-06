27a600a052cbd6da2066e0b2a30afe2793e3ba0e
kucherov_viktor <polkovnikotes@yandex.ru>
2021-05-06 15:28:46 +0300

    test for pull request

--- src/viktor/kucherov/Main.java
+++ src/viktor/kucherov/Main.java
@@ -3,6 +3,6 @@ package viktor.kucherov;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            System.out.println("test pull request");
     }
 }
